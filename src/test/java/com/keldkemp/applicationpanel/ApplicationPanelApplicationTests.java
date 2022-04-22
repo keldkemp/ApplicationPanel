6991bb7831bf22dfe780be6a3a16d6a50479e080
keldkemp <ilya2707004@yandex.ru>
2022-04-22 23:17:42 +0500

    add Dockerfile

--- src/test/java/com/keldkemp/applicationpanel/ApplicationPanelApplicationTests.java
+++ src/test/java/com/keldkemp/applicationpanel/ApplicationPanelApplicationTests.java
@@ -1,13 +1,4 @@
 package com.keldkemp.applicationpanel;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class ApplicationPanelApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
